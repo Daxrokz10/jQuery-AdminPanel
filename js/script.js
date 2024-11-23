b2000f93aa02e1b16ecde3291afb45da91c44dbc
Daxrokz10 <dakshgagnani@gmail.com>
2024-11-23 11:22:07 +0530

    fixed script

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 $("document").ready(function(){
 
     // NAVBAR
-    // const $button  = document.querySelector('#sidebar-toggle');
-    // const $wrapper = document.querySelector('#wrapper');
+    const $button  = document.querySelector('#sidebar-toggle');
+    const $wrapper = document.querySelector('#wrapper');
 
-    // $button.addEventListener('click', (e) => {
-    // e.preventDefault();
-    // $wrapper.classList.toggle('toggled');
-    // });
+    $button.addEventListener('click', (e) => {
+    e.preventDefault();
+    $wrapper.classList.toggle('toggled');
+    });
 
-    // // Initially show the first image
-    // $('#image1').show();
-    // $('#image2').hide();
+    // Initially show the first image
+    $('#image1').show();
+    $('#image2').hide();
 
-    // $('#sidebar-toggle').click(function() {
-    //     $('#sidebar-wrapper').toggleClass('toggled'); // Assuming you have a class for toggled state
-    //     if ($('#sidebar-wrapper').hasClass('toggled')) {
-    //         $('#nav-image-1').hide();
-    //         $('#nav-image-2').show();
-    //     } else {
-    //         $('#nav-image-1').show();
-    //         $('#nav-image-2').hide();
-    //     }
-    // });
+    $('#sidebar-toggle').click(function() {
+        $('#sidebar-wrapper').toggleClass('toggled'); // Assuming you have a class for toggled state
+        if ($('#sidebar-wrapper').hasClass('toggled')) {
+            $('#nav-image-1').hide();
+            $('#nav-image-2').show();
+        } else {
+            $('#nav-image-1').show();
+            $('#nav-image-2').hide();
+        }
+    });
 
     // // FARM METER
 
